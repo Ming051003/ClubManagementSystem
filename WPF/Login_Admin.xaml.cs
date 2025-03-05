789eae5fb6b54cf85ed2e946a0fd34ed564db6ea
bvmin <bvmin@BUIMINH>
2025-03-05 23:40:42 +0700

    Add project files.

---
+++ WPF/Login_Admin.xaml.cs
@@ -0,0 +1,59 @@
+﻿using BusinessObjects.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Services.BusinessService;
+using Services.Interface;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login_Admin : Window
+    {
+        private readonly IAdminService adminService;
+        public Login_Admin()
+        {
+            InitializeComponent();
+            adminService = ((App)Application.Current).ServiceProvider.GetRequiredService<IAdminService>() ?? throw new ArgumentNullException(nameof(AdminService));
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtUsername.Text == null || txtPassword.Text == null)
+            {
+                MessageBox.Show("Incorrect username/password", "Error Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                Admin? admin = await adminService.CheckLogin(txtUsername.Text, txtPassword.Text);
+                if (admin != null) {
+                    MessageBox.Show("Login Successfully!", "Information Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+            }
+        }
+        private void Border_MouseDown(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes) { 
+                this.Close();
+            }
+        }
+    }
+}
---
+++ WPF/Main_Admin_WPF.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WpfApp1.UC_Admin_WPF;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Main_Admin_WPF.xaml
+    /// </summary>
+    public partial class Main_Admin_WPF : Window
+    {
+        public Main_Admin_WPF()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void UC_Admin_Home_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UC_Admin_Home();
+        }
+
+        private void UC_Admin_Staff_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UC_Admin_Staff(); 
+        }
+
+        private void UC_Admin_Menu_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UC_Admin_Menu();
+        }
+
+        private void UC_Admin_WorkDay_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UC_Admin_WorkDay();
+        }
+
+        private void UC_Admin_Invoice_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UC_Admin_Invoice();
+        }
+
+        private void UC_Admin_Worktime_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content=new UC_Admin_Worktime();
+        }
+
+        private void UC_Admin_Inventory_Checked(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UC_Admin_Inventory(); 
+        }
+
+        
+    }
+}
