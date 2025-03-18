ee6fb80b71f8c67b21b8c905e27fd5efe40c3f00
bvmin <bvmin@BUIMINH>
2025-03-18 16:54:09 +0700

    mau ten thu muc

--- WPF/Login_Admin.xaml.cs
+++ WPF/Login_Admin.xaml.cs
@@ -1,7 +1,4 @@
-﻿using BusinessObjects.Models;
-using Microsoft.Extensions.DependencyInjection;
-using Services.BusinessService;
-using Services.Interface;
+﻿
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,40 +17,40 @@ namespace WpfApp1
     /// </summary>
     public partial class Login_Admin : Window
     {
-        private readonly IAdminService adminService;
+        //private readonly IAdminService adminService;
         public Login_Admin()
         {
             InitializeComponent();
-            adminService = ((App)Application.Current).ServiceProvider.GetRequiredService<IAdminService>() ?? throw new ArgumentNullException(nameof(AdminService));
+            //adminService = ((App)Application.Current).ServiceProvider.GetRequiredService<IAdminService>() ?? throw new ArgumentNullException(nameof(AdminService));
         }
 
         private async void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            if(txtUsername.Text == null || txtPassword.Text == null)
-            {
-                MessageBox.Show("Incorrect username/password", "Error Message", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            else
-            {
-                Admin? admin = await adminService.CheckLogin(txtUsername.Text, txtPassword.Text);
-                if (admin != null) {
-                    MessageBox.Show("Login Successfully!", "Information Message", MessageBoxButton.OK, MessageBoxImage.Information);
+            //if(txtUsername.Text == null || txtPassword.Text == null)
+            //{
+            //    MessageBox.Show("Incorrect username/password", "Error Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+            //else
+            //{
+            //    Admin? admin = await adminService.CheckLogin(txtUsername.Text, txtPassword.Text);
+            //    if (admin != null) {
+            //        MessageBox.Show("Login Successfully!", "Information Message", MessageBoxButton.OK, MessageBoxImage.Information);
 
-                }
-            }
+            //    }
+            //}
         }
         private void Border_MouseDown(object sender, MouseEventArgs e) {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
+            //if (e.LeftButton == MouseButtonState.Pressed)
+            //{
+            //    DragMove();
+            //}
         }
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.Yes) { 
-                this.Close();
-            }
+            //MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            //if (result == MessageBoxResult.Yes) { 
+            //    this.Close();
+            //}
         }
     }
 }
--- WPF/Main_Admin_WPF.xaml.cs
+++ WPF/Main_Admin_WPF.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows;
 using System.Windows.Input;
-using WpfApp1.UC_Admin_WPF;
 
 namespace WpfApp1
 {
@@ -47,40 +46,40 @@ namespace WpfApp1
             }
         }
 
-        private void UC_Admin_Home_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new UC_Admin_Home();
-        }
+        //private void UC_Admin_Home_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content = new UC_Admin_Home();
+        //}
 
-        private void UC_Admin_Staff_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new UC_Admin_Staff(); 
-        }
+        //private void UC_Admin_Staff_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content = new UC_Admin_Staff(); 
+        //}
 
-        private void UC_Admin_Menu_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new UC_Admin_Menu();
-        }
+        //private void UC_Admin_Menu_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content = new UC_Admin_Menu();
+        //}
 
-        private void UC_Admin_WorkDay_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new UC_Admin_WorkDay();
-        }
+        //private void UC_Admin_WorkDay_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content = new UC_Admin_WorkDay();
+        //}
 
-        private void UC_Admin_Invoice_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new UC_Admin_Invoice();
-        }
+        //private void UC_Admin_Invoice_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content = new UC_Admin_Invoice();
+        //}
 
-        private void UC_Admin_Worktime_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content=new UC_Admin_Worktime();
-        }
+        //private void UC_Admin_Worktime_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content=new UC_Admin_Worktime();
+        //}
 
-        private void UC_Admin_Inventory_Checked(object sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new UC_Admin_Inventory(); 
-        }
+        //private void UC_Admin_Inventory_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    MainContent.Content = new UC_Admin_Inventory(); 
+        //}
 
         
     }
